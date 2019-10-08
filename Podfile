7610195967aa4a1fb9c683ddb91f80a31bfe6bf4
Dan Ilca <iddan14@gmail.com>
2019-10-08 14:29:17 +0300

    First working Cocoa pod version

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.3'
+
+target 'SwiftShare' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftShare
+   pod 'Alamofire'
+   pod 'RxSwift'
+   pod 'SwiftyBeaver'
+
+  target 'SwiftShareTests' do
+    # Pods for testing
+  end
+
+end
---
+++ SwiftShare/AppDataError.swift
@@ -0,0 +1,71 @@
+//
+//  AppDataError.swift
+//  SwiftShare
+//
+//  Created by Dan ILCA on 08/10/2019.
+//  Copyright © 2019 Dan Ilca. All rights reserved.
+//
+
+import Foundation
+
+public enum AppDataError: Error {
+    case noData(reason: String)
+    case networkError(reason: String, code: Int, url: URL?)
+    case jsonParsingError(reason: String)
+    case missingToken(reason: String)
+    case cachingError(reason: String)
+    case unsupportedOperation(reason: String)
+    case configurationError(reason: String)
+    case unknownError(reason: String)
+
+    public var reason: String {
+        switch self {
+        case .noData(let msg):
+            return msg
+        case .networkError(let msg, _, _):
+            return msg
+        case .jsonParsingError(let msg):
+            return msg
+        case .missingToken(let msg):
+            return msg
+        case .cachingError(let msg):
+            return msg
+        case .unsupportedOperation(let msg):
+            return msg
+        case .configurationError(let msg):
+            return msg
+        case .unknownError(let msg):
+            return msg
+        }
+    }
+
+    public var shortDescription: String {
+        switch self {
+        case .noData:
+            return "noData"
+        case .networkError(_, let code, _):
+            return "networkError.\(code)"
+        case .jsonParsingError:
+            return "jsonParsingError"
+        case .missingToken:
+            return "missingToken"
+        case .cachingError:
+            return "cachingError"
+        case .unsupportedOperation:
+            return "unsupportedOperation"
+        case .configurationError:
+            return "configurationError"
+        case .unknownError:
+            return "unknownError"
+        }
+    }
+
+    public var code: Int? {
+        switch self {
+        case .networkError(_, let code, _):
+            return code
+        default:
+            return nil
+        }
+    }
+}
