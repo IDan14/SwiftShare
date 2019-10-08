1af117e9b66a7650d4f22bf1d7c4e7f9907d8ce5
Dan Ilca <iddan14@gmail.com>
2019-10-08 13:21:06 +0300

    Initial Commit

---
+++ SwiftShare/SwiftShare.h
@@ -0,0 +1,19 @@
+//
+//  SwiftShare.h
+//  SwiftShare
+//
+//  Created by Dan ILCA on 08/10/2019.
+//  Copyright © 2019 Dan Ilca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftShare.
+FOUNDATION_EXPORT double SwiftShareVersionNumber;
+
+//! Project version string for SwiftShare.
+FOUNDATION_EXPORT const unsigned char SwiftShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftShare/PublicHeader.h>
+
+
